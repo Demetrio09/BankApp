f46de3754e2531bc518f58d0e0f6498608874052
Demetrio Almeida <demetrioalmeida@gmail.com>
2020-10-12 08:42:56 -0400

    Bank application wrote in C#

---
+++ MyBank/BankAccount.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBank
+{
+    public class BankAccount
+    {
+
+        public string Number { get; }
+
+        public string Owner { get; set; }
+
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+
+        }
+
+        private static int accountNumberSeed = 1234567890;
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+            MakeDeposit(initialBalance, DateTime.Now, $"Initial balance ${initialBalance}");
+        }
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive!");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must not be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds fot this transaction!");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+        // Get account history
+        public string GetAcccountHistory()
+        {
+            var report = new StringBuilder();
+
+            decimal balance = 0;
+
+            // HEADER
+            report.AppendLine("Date\t\tAmount\tBalance\tNote");
+            foreach (var item in allTransactions)
+            {
+                balance += item.Amount;
+                // ROWS
+                report.AppendLine($"{item.Date.ToShortDateString()}\t${item.Amount}\t${balance}\t{item.Notes}");
+            }
+            return report.ToString();
+        }
+    }
+}
---
+++ MyBank/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Demetrio", 500);
+
+            Console.WriteLine($"Account number: {account.Number} open by {account.Owner} has initial balance of ${account.Balance}");
+
+            account.MakeWithdrawal(100, DateTime.Now, "dinner");
+            Console.WriteLine($"Withdrawal done! Your new balance is ${account.Balance}!");
+            account.MakeDeposit(200, DateTime.Now, "pay day");
+            Console.WriteLine($"Deposit done! My new balance is ${account.Balance}!");
+
+            // Geeting the account history.
+            Console.WriteLine(account.GetAcccountHistory());
+
+            // Test that the initial balance must be posite
+            try
+            {
+                var invalidAccount = new BankAccount("second account", 55);
+                Console.WriteLine($"New account number {invalidAccount.Number} initial balance {invalidAccount.Balance}");
+                Console.WriteLine(invalidAccount.GetAcccountHistory());
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("exeption caught creating an invalid account");
+                Console.WriteLine(e.ToString());
+            }
+
+        }
+    }
+}
